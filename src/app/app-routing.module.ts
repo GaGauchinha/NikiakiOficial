648483d522b3d5e1da256ca32c8c1bcff5d127b6
Maria Gabriela S. A <mgabrielaprofissional@hotmail.com>
2023-01-24 09:42:05 -0300

    Sem as permissões

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ListaReceitaComponent} from "./paginas/usuario/lista-receita/lista-receita.component";
+import {EditarReceitaComponent} from "./paginas/administrador/crud-receita/editar-receita/editar-receita.component";
+import {
+  RegistrarReceitaComponent
+} from "./paginas/administrador/crud-receita/registrar-receita/registrar-receita.component";
+import {RdUsuarioComponent} from "./paginas/administrador/rd-usuario/rd-usuario.component";
+import {CadastrarUsuarioComponent} from "./paginas/usuario/cadastrar-usuario/cadastrar-usuario.component";
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', redirectTo: 'lista-receita', pathMatch: 'full' },
+  { path: 'lista-receita', component: ListaReceitaComponent },
+  { path: 'editar-receita/:id', component: EditarReceitaComponent },
+  { path: 'registrar-receita', component: RegistrarReceitaComponent },
+  { path: 'rd-usuario', component: RdUsuarioComponent },
+  { path: 'rd-usuario/:id', component: RdUsuarioComponent },
+  { path: 'cadastrar-usuario', component: CadastrarUsuarioComponent }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrarReceitaComponent } from './paginas/administrador/crud-receita/registrar-receita/registrar-receita.component';
+import { EditarReceitaComponent } from './paginas/administrador/crud-receita/editar-receita/editar-receita.component';
+import { RdUsuarioComponent } from './paginas/administrador/rd-usuario/rd-usuario.component';
+import { CadastrarUsuarioComponent } from './paginas/usuario/cadastrar-usuario/cadastrar-usuario.component';
+import { ListaReceitaComponent } from './paginas/usuario/lista-receita/lista-receita.component';
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import { LoginComponent } from './paginas/login/login.component';
+import {authInterceptorProviders} from "./helpers/auth.interceptor";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrarReceitaComponent,
+    EditarReceitaComponent,
+    RdUsuarioComponent,
+    CadastrarUsuarioComponent,
+    ListaReceitaComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [authInterceptorProviders ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
